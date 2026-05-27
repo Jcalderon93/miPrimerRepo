735036299f10cbb49ead9e922451658d2c7526c3
Jcalderon93 <jcalderonreyes2018@gmail.com>
2026-05-27 11:33:41 -0500

    corrigiendo seguridad

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,4 +1,1 @@
 ﻿Console.WriteLine("Hello, platzi!");
-
-
-string stripe_api_key="sk_test_4eC39HqLyjwDarjt1zdp7dc";
--- probando/prueba-seguridad.js
+++
@@ -1,6 +0,0 @@
-// Simulación de credencial de producción expuesta para Secret Scanning
-const STRIPE_API_KEY = "sk_live_51NxA2B3C4D5E6F7G8H9I0J1K2L3M4N5O6P7Q8R9S0T";
-
-console.log("Validando detección de secretos en GitHub...");
-
-/ actualización de prueba
